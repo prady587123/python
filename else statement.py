dc481a0b08b9f3dbddcd8b21498a34b9ebff5ff3
prady587123 <pradyboy783@gmail.com>
2025-04-01 09:38:51 +0530

    Add files via upload

---
+++ else statement.py
@@ -0,0 +1,21 @@
+a = 20 
+b = 40 
+c = 0
+if a and b and c :
+    print("All the numbers have boolean value as true")
+else :
+    print("Atleast one number has boolean value as false")
+a = 10
+b = -10 
+c = 0
+if a > 0 or b > 0 :
+    print("Either of number is greater than 0")
+else :
+    print("No number is greater than 0")
+if b > 0 or c > 0 :
+    print("Either number is greater than 0")
+else : 
+    print("No number is greater than 0")
+
+
+
