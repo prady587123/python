895631a0e2ccb5b707bf6949d5f7cea663ee5dfd
prady587123 <pradyboy783@gmail.com>
2025-04-02 09:56:47 +0530

    Add files via upload

---
+++ grading system.py
@@ -0,0 +1,28 @@
+print("Enter your marks obtained in 5 subjects: ")
+markone = int(input())
+marktwo = int(input())
+markthree = int(input())
+markfour = int(input())
+markfive = int(input())
+total = markone + marktwo + markthree + markfour + markfive
+avg = total/5
+if avg >= 91 and avg <= 100:
+    print("your grade is A1")
+elif avg >= 81 and avg < 91:
+    print ("your grade is A2")
+elif avg >= 71 and avg < 81:
+    print ("your grade is B1")
+elif avg >= 61 and avg < 71:
+    print ("your grade is B2")
+elif avg >= 51 and avg < 61:
+    print ("your grade is C1")
+elif avg >= 41 and avg < 51:
+    print ("your grade is C2")
+elif avg >= 33 and avg < 41:
+    print ("your grade is D")
+elif avg >= 21 and avg < 33:
+    print ("your grade is E")
+elif avg >= 0 and avg < 21:
+    print ("your grade is F")
+else:
+    print ("Invalid input")
