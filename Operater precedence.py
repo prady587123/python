1af96760dd9e5135cea2bdc789364e8734cff387
prady587123 <pradyboy783@gmail.com>
2025-04-03 09:24:32 +0530

    Add files via upload

---
+++ Operater precedence.py
@@ -0,0 +1,13 @@
+a = 58
+b = 47
+c = 91
+d = 32
+e = 0
+z = (a+b) * a/b
+print("Value of (v+w) * x/y is",z)
+name = "messi"
+age = 0
+if name == "messi" or name  == "ronaldo" and age >=2:
+    print("Hello! Welcome.")
+else:
+    print("Good Bye")
