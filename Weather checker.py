32ae5f92b28d501f352675db00feb89a0ac0d583
prady587123 <pradyboy783@gmail.com>
2025-04-14 09:31:04 +0530

    Add files via upload

---
+++ Weather checker.py
@@ -0,0 +1,6 @@
+def weather_condition():
+    print('The weather is pleasent in : ', spring)
+    print('The weather is pleasent in : ', autumn)
+spring = "autumn"
+autumn = spring
+weather_condition()
