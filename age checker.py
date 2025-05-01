915e041379e34b8e92fecafddea666787667d7fe
prady587123 <pradyboy783@gmail.com>
2025-05-01 09:45:53 +0530

    Add files via upload

---
+++ age checker.py
@@ -0,0 +1,6 @@
+age = int(input("Enter age: "))
+
+if age >=18:
+    print ("You are a adult")
+else:
+    print("You are not a adult")
---
+++ sin cos trignometric value.py
@@ -0,0 +1,3 @@
+import math
+print(math.sin(math.radians(30)))
+print(math.sin(math.radians(90)))
