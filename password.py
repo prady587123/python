082c088b2427804bd4310a4921be5289485e2300
prady587123 <pradyboy783@gmail.com>
2025-05-09 08:06:39 +0530

    Add files via upload

---
+++ password.py
@@ -0,0 +1,7 @@
+import  random, string
+
+length = 13
+chars = string.ascii_letters + string.digits + '!@#$%^&*()'
+random.seed = (os.urandom(1024))
+
+print (random.choice(chars) for i in range(length))
